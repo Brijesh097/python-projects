e0a31a03e05eeed5a6a3f8461af7c5aecf1af710
Brijesh097 <brijesh.reddy15@siesgst.ac.in>
2023-02-18 02:08:44 -0500

    Day 01: Done

---
+++ day_01/band_name_generator.py
@@ -0,0 +1,11 @@
+# greeting message
+print("\nWelcome to the Band Name Generator\n")
+
+# get the name of the city
+city = input('What\'s the name of the city you grew up in?\n')
+
+# get the name of the pet
+pet_name = input('What\'s the name of your pet?\n')
+
+# concat the city name with the pet's name and output the result
+print('Your band name could be ' + city + ' ' + pet_name  + '\n')
