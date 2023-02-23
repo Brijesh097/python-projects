01498e7ad32d29ff39b677f16dca6df54ebe9853
Brijesh097 <brijesh.reddy15@siesgst.ac.in>
2023-02-23 13:49:00 -0500

    Created a program to find heads and tails

---
+++ day_04/heads_tails.py
@@ -0,0 +1,11 @@
+# import random module
+import random
+
+# set the start and end points
+random_side = random.randint(0, 1)
+
+# check the conditions
+if random_side == 1:
+    print("Heads")
+else:
+    print("Tails")
